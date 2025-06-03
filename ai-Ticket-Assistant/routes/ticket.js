583d633dbbaf5dcf5f7989e88a028470466b6ea4
Nakul <nakulverma.py@gmail.com>
2025-06-03 13:10:19 +0530

    feat: add ticket routes and integrate Inngest functions for ticket management

--- ai-Ticket-Assistant/routes/ticket.js
+++ ai-Ticket-Assistant/routes/ticket.js
@@ -1,3 +1,11 @@
-import express from 'express';
-import {authenticate} from '../middleware/auth.js';
-import { createTicket, getTicket, updateTicket } from '../controller/ticket.js';
+import express from "express";
+import { authenticate } from "../middleware/auth.js";
+import { createTicket, getTickets, getTicket } from "../controller/ticket.js";
+
+const router = express.Router();
+
+router.post("/create", authenticate, createTicket);
+router.get("/tickets", authenticate, getTickets);
+router.get("/ticket/:id", authenticate, getTicket);
+
+export default router;
