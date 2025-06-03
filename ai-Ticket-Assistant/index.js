1c962166ca58c15005d630c873fe24fafe517cde
Nakul <nakulverma.py@gmail.com>
2025-06-03 19:37:02 +0530

    feat: add dotenv configuration for environment variables and improve code formatting

--- ai-Ticket-Assistant/index.js
+++ ai-Ticket-Assistant/index.js
@@ -1,12 +1,15 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import cors from 'cors';
-import {serve} from 'inngest/express';
-import userRoutes from './routes/user.js';
-import ticketRoutes from './routes/ticket.js';
-import {inngest} from './inngest/client.js';
-import { onTicketCreation} from './inngest/functions/on-ticket-creation.js';
-import { onUserSignUp } from './inngest/functions/on-signUP.js';
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import { serve } from "inngest/express";
+import userRoutes from "./routes/user.js";
+import ticketRoutes from "./routes/ticket.js";
+import { inngest } from "./inngest/client.js";
+import { onTicketCreation } from "./inngest/functions/on-ticket-creation.js";
+import { onUserSignUp } from "./inngest/functions/on-signUP.js";
+
+import dotenv from "dotenv";
+dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 const app = express();
@@ -14,18 +17,23 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/auth', userRoutes);
-app.use('/api/tickets', ticketRoutes);
+app.use("/api/auth", userRoutes);
+app.use("/api/tickets", ticketRoutes);
 
-app.use( '/api/inngest',
-    serve({
-        client: inngest,
-        functions: [onTicketCreation, onUserSignUp],
-    }));
+app.use(
+  "/api/inngest",
+  serve({
+    client: inngest,
+    functions: [onTicketCreation, onUserSignUp],
+  })
+);
 
-mongoose.connect(process.env.MONGO_URL)
-.then(() => {
-    console.log('MongoDB connected successfully ✅');
-    app.listen(PORT, () => console.log(`Server is running on port ${process.env.PORT || 3000} 🚀`));
-})
-.catch((err) => console.error('❗❗MongoDB connection error :', err));
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("MongoDB connected successfully ✅");
+    app.listen(PORT, () =>
+      console.log(`Server is running at https://localhost:3000 🚀`)
+    );
+  })
+  .catch((err) => console.error("❗❗MongoDB connection error :", err));
